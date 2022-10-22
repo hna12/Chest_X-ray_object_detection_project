3c758bf4988068746858af4941184c67ec83ef40
hna12 <61971952+hna12@users.noreply.github.com>
2022-10-22 15:15:11 +0900

    Update augmentation.py

--- augmentation.py
+++ augmentation.py
@@ -61,24 +61,3 @@ def equ_image(path, image_list):
         img_name = path.split('/')[-1].split('.')[0]
         cv2.imwrite(f'{path}/{img_name}'+'_equ.png', equ_img)
 
-# CUTMIX
-def rand_bbox(size, lamb):
-    W = size[0]
-    H = size[1]
-    cut_rat = np.sqrt(1. - lamb)
-    cut_w = np.int(W * cut_rat)
-    cut_h = np.int(H * cut_rat)
-
-    # uniform
-    cx = np.random.randint(W)
-    cy = np.random.randint(H)
-
-    bbx1 = np.clip(cx - cut_w // 2, 0, W)
-    bby1 = np.clip(cy - cut_h // 2, 0, H)
-    bbx2 = np.clip(cx + cut_w // 2, 0, W)
-    bby2 = np.clip(cy + cut_h // 2, 0, H)
-
-    return bbx1, bby1, bbx2, bby2
-
-# MOSAIC
-
